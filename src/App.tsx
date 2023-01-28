a1a498083c53e01ab91e3d1e50f7bd413e452fc6
Mohamed Alaa <mohamedelmenyawe606@gmail.com>
2023-01-28 12:02:28 +0200

    finished with Question card for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React , {useState} from 'react';
 import QuestionCard from './component/QuestionCard';
 
 // https://opentdb.com/api.php?amount=10&type=multiple
+  const TOTAL_QUESTIONS = 10
+
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
 
   const startTrivia = async () =>{
 
-
   }
 
- // const chechAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
-
-
- // }
+  const chechAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+  
+  }
 
   const nextQuestion = () => {
 
@@ -23,7 +29,14 @@ function App() {
       <button className='start' onClick={startTrivia}>Start</button>
       <p className='score'>Score:</p>
       <p>Loading Question ...</p>
-      
+      <QuestionCard
+            questionNr={number + 1}
+            totalQuestions={TOTAL_QUESTIONS}
+            question={questions[number].question}
+            answers={questions[number].answers}
+            userAnswer={userAnswers ? userAnswers[number] : undefined}
+            callback={checkAnswer}
+          />
       <button className='next' onClick={nextQuestion}>Next Question</button>
     </div>
   );
