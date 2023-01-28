0f8264d165140376534e7a67e02908cd777350df
Mohamed Alaa <mohamedelmenyawe606@gmail.com>
2023-01-28 12:58:28 +0200

    finished work on API

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React , {useState} from 'react';
 import QuestionCard from './component/QuestionCard';
+import { fetchQuizQuestions } from './Api'; 
+import { Difficulty } from './Api';
 
-// https://opentdb.com/api.php?amount=10&type=multiple
+
   const TOTAL_QUESTIONS = 10
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
   const [userAnswers, setUserAnswers] = useState([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
+
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY))
 
   const startTrivia = async () =>{
 
@@ -29,14 +33,14 @@ function App() {
       <button className='start' onClick={startTrivia}>Start</button>
       <p className='score'>Score:</p>
       <p>Loading Question ...</p>
-      <QuestionCard
-            questionNr={number + 1}
+         {/* <QuestionCard
+           questionNr={number + 1}
             totalQuestions={TOTAL_QUESTIONS}
             question={questions[number].question}
             answers={questions[number].answers}
             userAnswer={userAnswers ? userAnswers[number] : undefined}
-            callback={checkAnswer}
-          />
+            callback={chechAnswer}
+          /> */}
       <button className='next' onClick={nextQuestion}>Next Question</button>
     </div>
   );
